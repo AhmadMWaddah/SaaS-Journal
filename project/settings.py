6dcad206431633b484b9609ad1f568b859846d08
AhmadMWaddah <wddh.ahmd@gmail.com>
2024-01-09 19:48:46 +0200

    Django Project
    Add a Django Project Core Directory.

--- project/settings.py
+++ project/settings.py
@@ -49,10 +49,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'saas_db',
+    'USER': 'amw',
+    'PASSWORD': '123',
+    'HOST': 'localhost',
+    'PORT': '',
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
